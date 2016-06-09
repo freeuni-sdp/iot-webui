fefb7469627402bd749639501a0fd10244bbddf6
ggham <ggham12@freeuni.edu.ge>
2016-06-09 22:21:02 +0400

    initial version of app

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var path = require('path')
+var app = express()
+
+// set directory for serving static content
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('*', function(req, res){
+	res.sendFile(__dirname + '/public/html/index.html')
+})
+
+// if deployed to heroku, port will be set from env variable,
+// while running locally, port will be 8080
+app.listen(process.env.PORT || 8080)
