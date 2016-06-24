275216217c758e99e7764606f0867df3e8f5ffd2
ggham <ggham12@freeuni.edu.ge>
2016-06-24 16:34:58 +0400

    added house registry functionality

---
+++ public/js/houses.js
@@ -0,0 +1,36 @@
+var housesApiRoutes = {
+  getAllHouses: 'http://private-anon-b483c7d04-iothouseregistry.apiary-mock.com/houses'
+}
+
+var currentlySelectedHouse = undefined;
+var housesList = undefined;
+
+function initHousesDropdown(result) {
+  housesList = result
+  var dropdownList = $('.dropdown-menu');
+  for (var i in housesList) {
+    if (i == 0) {
+      currentlySelectedHouse = housesList[i];
+      dropdownList.append('<div class="dropdown-item" id="selected">' + housesList[i].name._ + '</div>');
+    } else {
+      dropdownList.append('<div class="dropdown-item">' + housesList[i].name._ + '</div>');
+    }
+  }
+}
+
+$(document).ready(function() {
+  $('.dropdown .dropdown-item').click(function(){
+    $('.dropdown-item#selected').removeAttr('id');
+    $(this).attr('id', 'selected');
+    currentlySelectedHouse = housesList[$(this).index()];
+  });
+
+  $.ajax({
+    type: 'GET',
+    url: housesApiRoutes.getAllHouses,
+    dataType: 'json',
+    success: function(result) {
+      initHousesDropdown(result);
+    }
+  });
+})
