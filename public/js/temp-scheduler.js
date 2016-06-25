d6975784500c5b862d5fae8048745ae805ad8656
EvgeniOrmotsadze <eormo11@freeuni.edu.ge>
2016-06-25 22:21:00 +0400

    set temperature scheduler

--- public/js/temp-scheduler.js
+++ public/js/temp-scheduler.js
@@ -4,19 +4,52 @@
 var currentHouse = 1234;
 var floor = 1;
 var temperatureScheduler = "https://private-09e97-iottemperaturescheduler.apiary-mock.com/webapi/houses/"
-                            + currentHouse + "/floors/" + floor + "/schedule?";
+                            + currentHouse + "/floors/";
+var setTemperatureScheduler = "https://private-09e97-iottemperaturescheduler.apiary-mock.com/webapi/houses/"
+                             + currentHouse + "/floors/";
 
 $(document).ready(function() {
 
     $("#submit").click(function (){
         var stardate = $("#start-date").val() + ":00";
-        var enddate = $("#end-date").val() + ":00";
+        stardate = stardate.replace("T"," ");
+        var enddate = $("#end-date").val() + ":00"
+        enddate = stardate.replace("T"," ");
+        var floor = $("#floors").val();
         $.ajax({
             type: 'GET',
-            url: temperatureScheduler + "start?"+stardate +"&end?"+enddate,
+            url: temperatureScheduler +  floor + "/schedule?start?"+stardate +"&end?"+enddate,
             dataType: 'json',
             success: function(res) {
                 drawScheduler(res);
+            }
+        });
+    });
+
+    $("#submitSet").click(function (){
+        var stardate = $("#start-dateSet").val() + ":00";
+        stardate = stardate.replace("T"," ");
+        var enddate = $("#end-dateSet").val() + ":00";
+        enddate = enddate.replace("T"," ");
+        var floor = $("#floors").val();
+        var temp = $("#temperature").val();
+        var data = {
+            dateFrom : stardate,
+            dateTo : enddate,
+            temperature : temp
+        }
+        $.ajax({
+            type: 'POST',
+            url: temperatureScheduler +  floor,
+            data: data,
+            dataType: 'json',
+            success: function(res) {
+               alert("Schedule Updated !");
+            },
+            complete: function(res){
+                if(res.status == 200){
+                    alert("Schedule Updated !");
+                }
             }
         });
     });
