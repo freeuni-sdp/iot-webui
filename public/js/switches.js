edce7369c2bbebc214ea523a064a59624c767772
EvgeniOrmotsadze <eormo11@freeuni.edu.ge>
2016-06-25 15:57:31 +0400

    heating switch on/off

--- public/js/switches.js
+++ public/js/switches.js
@@ -5,14 +5,17 @@ var currentHouse = 1234;
 var sprinklerSwitchState = "https://private-anon-0daf60376-sprinklerswitch.apiary-mock.com/webapi/houses/" + currentHouse;
 var sprinklerSwitchChangeState = "https://private-anon-c802c2370-sprinklerswitch.apiary-mock.com/webapi/houses/" + currentHouse;
 var houseHeatingSwitch = "http://private-anon-293e85fef-iotheatingswitch.apiary-mock.com/house/" + currentHouse;
+var houseHeatingOn = "https://private-anon-293e85fef-iotheatingswitch.apiary-mock.com/house/" + currentHouse + "/floor/";
+var houseHeatingOff ="https://private-anon-293e85fef-iotheatingswitch.apiary-mock.com/house/" + currentHouse  + "/floor/";
+
 var sprinklerSwitchStateParams = {
     set_status: "on",
     timeout: 60
 };
 
 $(document).ready(function() {
-    sendAjax('GET',sprinklerSwitchState,'');
-    sendAjax('GET',houseHeatingSwitch,'');
+    sendAjax('GET',sprinklerSwitchState,'',"sprinklerSwitchState");
+    sendAjax('GET',houseHeatingSwitch,'',"houseHeatingSwitch");
     $("#sprinkler-switch-change-state").change(function () {
         if($(this).attr('checked')){
             sprinklerSwitchStateParams.set_status = "on";
@@ -20,31 +23,59 @@ $(document).ready(function() {
             sprinklerSwitchStateParams.set_status = "off"
         }
         sprinklerSwitchStateParams.timeout = $("#sprinkler-switch-change-time").val() == '' ? 60 : $("#sprinkler-switch-change-time").val()
-        sendAjax('PUT',sprinklerSwitchChangeState,sprinklerSwitchStateParams);
+        sendAjax('PUT',sprinklerSwitchChangeState,sprinklerSwitchStateParams,"sprinklerSwitchChangeState");
+    });
+
+    $(".checkbox").change(function(){
+        var id = $(this)[0].id;
+        var number = id.substr(id.length-1,1);
+        if($(this)[0].checked){
+            var data = {};
+            var period = $("#heater-floor1-n-" +number).val();
+            data.period = period == '' ? 600 : period;
+            $.ajax({
+                type: 'PUT',
+                url: houseHeatingOn + number,
+                dataType: 'json',
+                data:data,
+                complete : function(){
+                    sendAjax('GET',houseHeatingSwitch,'',"houseHeatingSwitch");
+                }
+            });
+        }else{
+            $.ajax({
+                type: 'DELETE',
+                url: houseHeatingOff + number,
+                complete : function(){
+                    sendAjax('GET',houseHeatingSwitch,'',"houseHeatingSwitch");
+                }
+            });
+        }
     });
 });
 
-function sendAjax(method,link,params) {
+function sendAjax(method,link,params,type) {
     $.ajax({
         type: method,
         url: link,
         dataType: 'json',
         data: params,
         success: function (result) {
-            initial(result,link);
+            console.log(link);
+            initial(result,type);
         }
     });
 }
 
-function initial(result,url){
-    switch (url) {
-        case sprinklerSwitchState:
+function initial(result,type){
+    switch (type) {
+        case "sprinklerSwitchState":
             getSprinklerStatus(result);
             break;
-        case sprinklerSwitchChangeState:
+        case "sprinklerSwitchChangeState":
             setSprinklerStatus();
             break;
-        case houseHeatingSwitch:
+        case "houseHeatingSwitch":
             getHouseHeatingInfo(result);
             break;
         default :
@@ -57,6 +88,10 @@ function setSprinklerStatus(){
     sendAjax('GET',sprinklerSwitchState,'');
 }
 
+function updateHeatingInfo(result){
+
+}
+
 function getSprinklerStatus(result) {
     $('#sprinkler-switch-current-state').html(result.status);
     $('#sprinkler-switch-current-time').html(result.seconds_left);
@@ -65,7 +100,7 @@ function getSprinklerStatus(result) {
 
 
 function getHouseHeatingInfo(result){
-  //  console.log(result);
+
     var table =  $("#heating-switch-tb");
     table.empty();
     for (let value of result.values){
